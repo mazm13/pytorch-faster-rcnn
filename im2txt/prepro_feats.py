a3b3124cfa38d49493d21ceca3018a26b625f68b
mazm13 <joeminmar@yeah.net>
2018-04-12 12:43:50 +0800

    changed the way of distill features with referring bottom-up codes

--- im2txt/prepro_feats.py
+++ im2txt/prepro_feats.py
@@ -7,6 +7,7 @@ import _init_paths
 from model.test import test_net
 from model.config import cfg, cfg_from_file, cfg_from_list
 from model.test import im_detect
+from model.nms_wrapper import nms
 from datasets.factory import get_imdb
 import argparse
 import pprint
@@ -18,7 +19,6 @@ from nets.mobilenet_v1 import mobilenetv1
 
 import numpy as np
 import torch
-
 
 if __name__ == "__main__":
     saved_model = saved_model = os.path.join('..', 'output', 'default.pth')
@@ -52,10 +52,36 @@ if __name__ == "__main__":
         os.mkdir(dir_det)
 
     images_root = os.path.join('..', 'data', 'coco', 'images')
+
+    NMS_THRESH = 0.3
+    conf_thresh = 0.2
+    MIN_BOXES = 36
+    MAX_BOXES = 36
+
     for i, img in enumerate(imgs):
         I = cv2.imread(os.path.join(images_root, img['filepath'], img['filename']))
         scores, boxes, fc7 = im_detect(net, I)
+
+        max_conf = np.zeros((scores.shape[0]))
+        for cls_ind in range(1, 81):
+            cls_boxes = boxes[:, 4 * cls_ind:4 * (cls_ind + 1)]
+            cls_scores = scores[:, cls_ind]
+            dets = np.hstack((cls_boxes, cls_scores[:, np.newaxis])).astype(np.float32)
+            keep = nms(torch.from_numpy(dets), NMS_THRESH)
+            max_conf[keep] = np.where(cls_scores[keep] > max_conf[keep], cls_scores[keep], max_conf[keep])
+
+        keep_boxes = np.where(max_conf >= conf_thresh)[0]
+        if len(keep_boxes) < MIN_BOXES:
+            keep_boxes = np.argsort(max_conf)[::-1][:MIN_BOXES]
+        elif len(keep_boxes) > MAX_BOXES:
+            keep_boxes = np.argsort(max_conf)[::-1][:MAX_BOXES]
+
+        fc7 = fc7[keep_boxes]
+        # values = np.max(scores, axis=1)
+        # inds = np.argsort(values)
+        # fc7 = fc7[inds[:32], :]
+
         np.savez_compressed(os.path.join(dir_det, str(img['cocoid'])), feat=fc7.data.cpu().float().numpy())
-        if i % 1000 == 0:
+        if i % 100 == 0:
             print('processing %d/%d (%.2f%% done)' % (i, N, i * 100.0 / N))
     print('wrote ', output_dir)
